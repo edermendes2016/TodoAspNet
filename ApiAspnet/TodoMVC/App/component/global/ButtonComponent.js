94abad7d923fa4724a7cb0950a484e6036964802
Tesla <Tesla@teste.com>
2019-02-02 10:49:31 -0300

    inclusão view tarefa usando vuejs

---
+++ ApiAspnet/TodoMVC/App/component/global/ButtonComponent.js
@@ -0,0 +1,11 @@
+﻿// Definindo novo componente chamado button-counter
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">Você clicou em mim {{ count }} vezes.</button>'
+})
+
+new Vue({ el: '#components-demo' })
---
+++ ApiAspnet/TodoMVC/App/component/global/adicionarTarefaComponent.js
@@ -0,0 +1,29 @@
+﻿// Comando 1
+var data = {
+    tarefas: [{ titulo: 'Teste titulo', descricao: 'Descricao', checked: true },
+        { titulo: 'Teste titulo 2', descricao: 'Descricao', checked: false }],
+    cabecalho: 'Suas tarefas',
+    novoTitulo : '',
+    novaDescricao: ''
+};
+// Comando 2
+new Vue({
+    el: '#tarefaId',
+    data: data,
+    methods: {
+        addTarefa: function () {            
+            var titulo = this.novoTitulo.trim();
+            var descricao = this.novaDescricao.trim();
+            if (titulo && descricao) {
+                this.tarefas.push({
+                    titulo: titulo,
+                    descricao: descricao,
+                    checked: false
+                });
+                this.novoTitulo = '';
+                this.novaDescricao = '';
+            }
+        }
+    }
+});
+
